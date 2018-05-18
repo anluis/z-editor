af5a21034db2896af7adc167128a8a63d040d303
Anluis <aforanluis@gmail.com>
2018-05-18 16:37:44 +0800

    fuck

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import Costum from '../components/costum'
+import { bindActionCreator } from 'redux'
 class Main extends React.Component {
   constructor() {
     super()

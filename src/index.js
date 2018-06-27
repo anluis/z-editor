897bff2875be82d906e7edefbdc5419277aa5aae
Anluis <aforanluis@gmail.com>
2018-06-27 11:14:16 +0800

    add logger and change title

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+/**
+ * Logs all actions and states after they are dispatched.
+ */
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store}>

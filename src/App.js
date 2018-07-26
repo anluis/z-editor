faf872c6e55a0cba2cd74da3c782d83b8f5ece3a
Anluis <aforanluis@gmail.com>
2018-07-26 15:56:15 +0800

    rescedule

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ComCollections from '../containers/ComCollections/ComCollections'
-import MenuBar from '../containers/MenuBar/MenuBar'
-import TopBar from '../containers/TopBar/TopBar'
-import FunctionTabs from '../containers/FunctionTabs/FunctionTabs'
-import '../assets/style/App.less'
+import ComCollections from './containers/ComCollections/ComCollections'
+import MenuBar from './containers/MenuBar/MenuBar'
+import TopBar from './containers/TopBar/TopBar'
+import FunctionTabs from './containers/FunctionTabs/FunctionTabs'
+import './assets/style/App.less'
 import 'normalize.css'
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store/configStore'
-import Root from './containers/Root'
+import Root from './Root'
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 registerServiceWorker()

f93403152d27d9d59f87aa46bf9ae030c9fe11a8
Joy <joywangx@sina.com>
2018-08-09 11:51:01 +0800

    属性

--- src/components/FunctionTabs/Attribute/Attribute.js
+++ src/components/FunctionTabs/Attribute/Attribute.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Input, Button, Modal } from 'antd'
+import Image from './Image'
+import Link from './Link'
+import Extend from './Extend'
+import Opacity from './Opacity'
+import { BACKGROUND_MODULE } from '../../../constants/ModuleTypes'
+
 const confirm = Modal.confirm
 
 const showDeleteConfirm = (deleteCom, id, targetPageId) => {
   confirm({
-    title: 'Are you sure delete this task?',
-    content: 'Some descriptions',
-    okText: 'Yes',
+    title: '你要删除这个组件吗?',
+    okText: '是',
     okType: 'danger',
-    cancelText: 'No',
+    cancelText: '否',
     onOk() {
       deleteCom(id, targetPageId)
       console.log('OK')
@@ -23,87 +28,40 @@ const Attribute = ({ focusCom, updateCom, deleteCom, status }) => {
   if (focusCom === undefined) {
     return null
   } else {
+    const displayStyle = {
+      display: 'none'
+    }
+    if (focusCom.attribute.type === BACKGROUND_MODULE) {
+      displayStyle.display = 'block'
+    } else {
+      displayStyle.display = 'none'
+    }
     return (
       <div className="attributes">
-        <div className="attr-item id">组件Id: {focusCom.id}</div>
-        <div className="attr-item name">
-          组件名称:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                name: e.target.value
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.name}
+        <div className="attributes-base">
+          <div className="attr-item id">组件Id: {focusCom.id}</div>
+          <div className="attr-item name">
+            组件名称:
+            <Input
+              onChange={e => {
+                let updatedAttr = {
+                  ...focusCom.attribute,
+                  name: e.target.value
+                }
+                updateCom(focusCom.id, updatedAttr)
+              }}
+              value={focusCom.attribute.name}
+            />
+          </div>
+          <Image updateCom={updateCom} focusCom={focusCom} />
+          <Link updateCom={updateCom} focusCom={focusCom} />
+          <Opacity
+            updateCom={updateCom}
+            focusCom={focusCom}
+            style={displayStyle}
           />
         </div>
-        <div className="attr-item x">
-          组件X:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                x: Number(e.target.value)
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.x}
-          />
-        </div>
-        <div className="attr-item y">
-          组件Y:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                y: Number(e.target.value)
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.y}
-          />
-        </div>
-        <div className="attr-item width">
-          组件宽度:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                width: Number(e.target.value)
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.width}
-          />
-        </div>
-        <div className="attr-item height">
-          组件高度:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                height: Number(e.target.value)
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.height}
-          />
-        </div>
-        <div className="attr-item img">
-          图片:
-          <Input
-            onChange={e => {
-              let updatedAttr = {
-                ...focusCom.attribute,
-                imgUrl: e.target.value
-              }
-              updateCom(focusCom.id, updatedAttr)
-            }}
-            value={focusCom.attribute.imgUrl}
-          />
-        </div>
+        <Extend updateCom={updateCom} focusCom={focusCom} />
         <div className="attr-item delete">
           <div>
             <Button

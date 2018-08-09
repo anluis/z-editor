9b1e89813b8a8dc6ecceb7bbab85179128e392e0
Anluis <aforanluis@gmail.com>
2018-08-09 13:57:35 +0800

    style fix

--- src/components/FunctionTabs/Pages/Pages.js
+++ src/components/FunctionTabs/Pages/Pages.js
@@ -30,8 +30,12 @@ const Pages = ({ pages, updatePageOrder, addPage, focusPage }) => {
     updatePageOrder(oldIndex, newIndex)
     focusPage(oldIndex)
   }
+  const pStyle = {
+    height: '100%',
+    overflowY: 'scroll'
+  }
   return (
-    <div>
+    <div style={pStyle}>
       <SortableList items={pages} onSortEnd={onSortEnd} />
       <Button onClick={() => addPage()}>新增</Button>
     </div>

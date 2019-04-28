ac9e57a2244d902cbd629d0274f892b12a2013c6
Anluis <zhengwending@jingfree.com>
2019-04-28 18:30:46 +0800

    save

--- src/components/FunctionTabs/Layers/Layers.tsx
+++ src/components/FunctionTabs/Layers/Layers.tsx
@@ -8,6 +8,11 @@ import { ThunkDispatch } from 'redux-thunk'
 import { deleteCom, updateCom } from '../../../actions/coms'
 import IStoreState from '../../../types/IStoreState';
 import styles from './Layers.module.css'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 interface StateProps {
   currentComs: Coms
@@ -15,41 +20,78 @@ interface StateProps {
 }
 
 interface DispatchProps {
-  deleteCom: (id: number, currentPageId: number) => void
+  deleteCom: (id: number, targetPageId: number) => void
   updateCom: (id: number, com: Com) => void
+}
+
+interface State {
+  deleteDialogOpen: boolean
+  choosenCom: Com | null
 }
 
 type Props = StateProps & DispatchProps
 
-const SortableItem = SortableElement(({ item, currentPageId, deleteCom }: { item: Com, currentPageId: number, deleteCom: (id: number, currentPageId: number) => void }) => {
+const SortableItem = SortableElement(({ item, currentPageId, handleDialogOpen }: { item: Com, currentPageId: number, handleDialogOpen: (com: Com) => void }) => {
   return (
     <div className={styles.layerItem}>
       {item.name}
-      <Button>设置</Button>
-      <Button onClick={() => deleteCom(item.id, currentPageId)}>删除</Button>
+      <Button variant="outlined">设置</Button>
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={(e) => {
+          e.preventDefault()
+          handleDialogOpen(item)
+        }}
+      >
+        删除
+      </Button>
     </div>
   )
 })
 
-const SortableList = SortableContainer(({ items, currentPageId, deleteCom }: { items: Coms, currentPageId: number, deleteCom: (id: number) => void }) => {
+const SortableList = SortableContainer(({ items, currentPageId, handleDialogOpen }: { items: Coms, currentPageId: number, handleDialogOpen: (com: Com) => void }) => {
   return (
     <div className={styles.layers}>
       {items.map((item, index) => {
-        <SortableItem
-          item={item}
-          key={`sortableItem-${index}`}
-          index={index}
-          currentPageId={currentPageId}
-          deleteCom={deleteCom}
-        />
+        return (
+          <SortableItem
+            // need to process Here
+            // disabled
+            item={item}
+            key={`sortableItem-${index}`}
+            index={index}
+            currentPageId={currentPageId}
+            handleDialogOpen={handleDialogOpen}
+          />
+        )
       })}
     </div>
   )
 })
 
-class Layers extends React.Component<Props> {
+class Layers extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
+    this.state = {
+      deleteDialogOpen: false,
+      choosenCom: null
+    }
+  }
+
+  handleDialogOpen = (item: Com) => {
+    console.log(22)
+    this.setState({ deleteDialogOpen: true, choosenCom: item })
+  }
+
+  hanldeDialogClose = () => {
+    this.setState({ deleteDialogOpen: false })
+    const { currentPageId, } = this.props
+    const { choosenCom } = this.state
+    if (!choosenCom) {
+      return
+    }
+    this.props.deleteCom(choosenCom.id, currentPageId)
   }
 
   onSortEnd = ({ oldIndex, newIndex }: { oldIndex: number, newIndex: number }) => {
@@ -62,14 +104,38 @@ class Layers extends React.Component<Props> {
 
 
   render() {
-    const { currentComs } = this.props
+    const { currentComs, currentPageId } = this.props
     return (
-      // <SortableList
-      //   items={currentComs}
-      //   onSortEnd={this.onSortEnd}
-      //   deleteCom={this.deleteCom}
-      // />
-      null
+      <>
+        <SortableList
+          items={currentComs}
+          currentPageId={currentPageId}
+          onSortEnd={this.onSortEnd}
+          handleDialogOpen={this.handleDialogOpen}
+          pressThreshold={20}
+        />
+        <Dialog
+          open={this.state.deleteDialogOpen}
+          onClose={this.hanldeDialogClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">前方高能预警！</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              你确实要删除组件{this.state.choosenCom ? this.state.choosenCom.name : ''}吗？
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.hanldeDialogClose} color="primary">
+              取消
+            </Button>
+            <Button onClick={this.hanldeDialogClose} color="primary" autoFocus>
+              确定
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
     )
   }
 }

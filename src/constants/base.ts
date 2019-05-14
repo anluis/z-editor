cf428918fd2606a7e1a9dece1421f9e43d079043
Anluis <zhengwending@jingfree.com>
2019-05-14 16:19:54 +0800

    fix share auth url

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -1,7 +1,7 @@
 export const cdnUrl = 'http://cdn.xingstation.cn'
 export const apiUrlStaging = 'http://cms.xingstation.net/api'
 export const apiUrlProduction = 'http://cms.xingstation.com/api'
-export const wxApiUrlStaging = 'http://sapi.jingfree.top/api'
+export const wxApiUrlStaging = 'http://sapi.xingstation.net/api'
 export const wxApiUrlProduction = 'http://sapi.xingstation.com/api'
 export const apiUrl = process.env.REACT_APP_STAGING ? apiUrlStaging : (process.env.NODE_ENV === 'production' ? apiUrlProduction : apiUrlStaging)
 export const wxApiUrl = process.env.REACT_APP_STAGING ? wxApiUrlStaging : (process.env.NODE_ENV === 'production' ? wxApiUrlProduction : wxApiUrlStaging)

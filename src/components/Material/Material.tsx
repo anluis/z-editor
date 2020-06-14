c4d720706f5954a6fd3b3e998163acece6f5a591
Anluis <wending.zheng@gmail.com>
2020-06-14 12:03:08 +0800

    update

--- src/components/Material/Material.tsx
+++ src/components/Material/Material.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
